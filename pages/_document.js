bc4c2a43d1a2498aacecf9497a9375b315e6b1d2
samertarcha <samertarcha@outlook.com>
2023-02-25 00:14:56 +0200

    added keywords tag

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,7 @@ class MyDocument extends Document {
 						name='description'
 						content='As a Front End developer, I specialize in building web applications with ReactJS, NextJS, TypeScript, SCSS, TailwindCSS, and GIT. With industry experience, I create high-quality and responsive web solutions.'
 					/>
+					<meta name="keywords" content="Samer Tarcha, Front-End, Developer, React, Software, Website" />
 				</Head>
 				<body>
 					<Main />
